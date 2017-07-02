abd5fe3aba152e7dfd5259fb3b28dea36413b5c4
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-02 11:00:19 +0200

    Add reducer auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
+import { AUTH_USER, UNAUTH_USER } from './types';
+
 
 const ROOT_URL = 'http://localhost:3090';
-
-
 
 export function signinUser({email, password, history}){
 
     return function (dispatch) {
         axios.post(`${ROOT_URL}/signin`, {email, password})
             .then(response => {
+
+                // Update state to true to show is authenticated
+                dispatch({type: AUTH_USER});
+
+                // Redirect to feature in case of success
                 history.push('/feature');
             })
             .catch(() => {
---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,14 @@
+import { AUTH_USER, UNAUTH_USER } from './types';
+
+
+export default function(state={}, action){
+
+    switch (action.type){
+    case AUTH_USER:
+        return {...state, authenticated: true};
+    case UNAUTH_USER:
+        return {...state, authenticated: false};
+
+    }
+    return state;
+}
