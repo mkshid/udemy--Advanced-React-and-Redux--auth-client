0b732859e72c2f2a39f3594e207b44f30d510b76
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-02 13:05:47 +0200

    Add sync validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -5,7 +5,35 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type}  className="form-control"/>
+      {touched && ((error && <span className='error'>{error}</span>))}
+    </div>
+  </div>
+);
+
+const validate = values => {
+    const errors = {};
+
+    if (values.password !== values.passwordConfirm){
+        errors.password = 'Password must be same!';
+    }
+
+    return errors;
+
+};
+
+
 class Signup extends Component {
+
     render(){
 
         const { handleSubmit } = this.props;
@@ -15,25 +43,21 @@ class Signup extends Component {
               <div className="form-group">
                 <label>Email: </label>
                 <div>
-                  <Field name="email" component="input"
-                         className="form-control"
-                         type="text" />
+                <Field name="email" component={renderField} type="text" />
                 </div>
               </div>
 
               <div className="form-group">
                 <label>Password: </label>
                 <div>
-                  <Field name="password" component="input" type="password"
-                         className="form-control"/>
+                  <Field name="password" component={renderField} type="password"/>
                 </div>
               </div>
 
               <div className="form-group">
                 <label>Confirm Password: </label>
                 <div>
-                  <Field name="passwordConfirm" component="input" type="password"
-                         className="form-control"/>
+                  <Field name="passwordConfirm" component={renderField} type="password"/>
                 </div>
               </div>
 
@@ -46,5 +70,6 @@ class Signup extends Component {
 }
 
 export default reduxForm(
-    {form: 'signup'}
+    {form: 'signup',
+    validate}
 )(Signup);
