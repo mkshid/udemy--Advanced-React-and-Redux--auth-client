de92a3d19b9c0d86bfbef299cfa18d9cd31b7e60
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-02 14:42:30 +0200

    Add check of token before rendering anything

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import reducers from './reducers';
 
+import { AUTH_USER } from './actions/types';
+
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if (token) {
+    // update the state
+    store.dispatch({type: AUTH_USER});
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <div>
         <Route path="/" component={App} />
