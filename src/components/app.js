a7fd28e0f38a6dc932284156d06567fc1b595945
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-02 14:37:04 +0200

    Add root url component

--- src/components/app.js
+++ src/components/app.js
@@ -13,7 +13,12 @@ export default class App extends Component {
     render() {
         return (
             <div>
-              <Header />
+                <Header />
+              <Route
+                 exact
+                 path='/'
+                 component={() => <div> Welcome to auth app </div>}
+                 />
               <Route
                  path={this.props.match.url + 'signin'}
                  component={Signin}
